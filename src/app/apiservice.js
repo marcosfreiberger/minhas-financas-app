a2ee869211e1c2d3aa0a4358133b36d40d58acc5
Freiberger <marcos.freiberger@capgemini.com>
2022-03-03 11:32:34 -0300

    Adicionando a url da api remota

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-  baseURL: 'http://localhost:8180',
+  baseURL: 'https://minhasfinancasback-api.herokuapp.com/',
 });
 
 class ApiService {
